5bf60eb884133af4cfbd71923c69218cc712651a
Aaron Geller <aaron.s.geller@gmail.com>
2025-11-20 19:20:10 -0700

    Removed unnecessary call to process_set_seeg, adjusted comments, and added setting group for misc channels.

--- create_brainstorm_subj.m
+++ create_brainstorm_subj.m
@@ -68,19 +68,20 @@ if ~noeeg
                                'channelreplace', 0, 'channelalign',   0);
 
         % edit channelfile
-        % 1) set type to SEEG
-        bst_process('CallProcess', 'process_channel_setseeg', sFileRaw, []);
 
         % Get channel file
         [sStudy, iStudy] = bst_get('ChannelFile', sFileRaw.ChannelFile);
         ChannelMat = in_bst_channel(sFileRaw.ChannelFile);
-        misc_channels = {'EKG1', 'Annotations', 'SpO2', 'EtCO2', 'Pulse', 'CO2Wave'};
+        misc_channels = {'EKG', 'EKG1', 'Annotations', 'SpO2', ...
+                         'EtCO2', 'Pulse', 'CO2Wave', 'BP1', 'BP2'};
         set_to_misc_str = '';
+        misc_inds = [];
         isfirst = 1;
         for i=1:length(ChannelMat.Channel)
-            % 2) find channels named EKG1, Annotations, SpO2, EtCO2, Pulse, CO2Wave
+            % 1) find channels named EKG1, Annotations, SpO2, EtCO2, Pulse, CO2Wave
             for j=1:length(misc_channels)
-                if strcmp(ChannelMat.Channel(i).Name, misc_channels(j))
+                if strcmp(ChannelMat.Channel(i).Name, misc_channels{j})
+                    misc_inds(end+1) = i;
                     if isfirst
                         isfirst = 0;
                     else
@@ -91,8 +92,9 @@ if ~noeeg
                 end
             end
 
-            % 3) find channels starting with $, rename them and set to MISC
+            % 2) find channels starting with $, rename them and set to MISC
             if strcmp(ChannelMat.Channel(i).Name(1), "$")
+                misc_inds(end+1) = i;
                 newname = ['XXX' int2str(i)];
                 ChannelMat.Channel(i).Name = newname;
                 if isfirst
@@ -104,8 +106,11 @@ if ~noeeg
             end
         end
 
-        bst_save(file_fullpath(sFileRaw.ChannelFile), ChannelMat, 'v7', 1);
+        % 3) set channel Group
+        [ChannelMat.Channel(misc_inds).Group] = deal('MISC');
+        bst_save(file_fullpath(sFileRaw.ChannelFile), ChannelMat, 'v7');
 
+        % 4) set channel Type
         bst_process('CallProcess', 'process_channel_settype', sFileRaw, [], ...
                     'sensortypes', set_to_misc_str, 'newtype', 'MISC');
     end
