2f60c88d635df4a21ad9f0c2e3b393530cc7aa0c
Aaron Geller <aaron.s.geller@gmail.com>
2026-02-28 15:53:58 -0700

    Only import files that haven't been imported already.

--- review_raw_all.m
+++ review_raw_all.m
@@ -1,13 +1,20 @@
-function review_raw_all(subj, norename)
+function review_raw_all(subj, protocolname, norename)
+
+if ~exist('protocolname', 'var')
+    protocolname = 'IEEG_visualization';
+end
+
+if ~exist('norename', 'var')
+    norename = 0;
+end
 
 fsdir = '/Applications/freesurfer/subjects';
 
 fssubjdir = fullfile(fsdir, subj);
 eegdir = fullfile(fssubjdir, 'eeg');
-
-if ~exist('norename', 'var')
-    norename = 0;
-end
+bsdatadir = fullfile('/Users/aaron/Documents/brainstorm_db/', ...
+                     protocolname, 'data');
+subjbsdatadir = fullfile(bsdatadir, subj);
 
 if ~norename
     % anonymize EDF file
@@ -17,7 +24,15 @@ end
 
 eegfiles = dir(fullfile(eegdir, '*.edf'));
 
-for k=1:length(eegfiles)    
-    % review raw EEG file
-    review_raw_edf(subj, fullfile(eegdir, eegfiles(k).name));
+for k=1:length(eegfiles)
+    thisfile = eegfiles(k).name;
+    [~, fstem, ~] = fileparts(thisfile);
+
+    % check that it's not already imported
+    if exist(fullfile(subjbsdatadir, ['@raw' fstem]), 'dir')
+        display(['Skipping ' thisfile '...']);
+    else
+        % review raw EEG file
+        review_raw_edf(subj, fullfile(eegdir, thisfile));
+    end
 end
